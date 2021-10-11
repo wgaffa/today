570f8189a012c5f751b115709fa161d55f67c943
Patrik Maunus <subscription@skriptladan.se>
2021-10-11 06:25:49 +0200

    Add prompts to create a Task

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,94 @@
+use std::error::Error;
 use today::Task;
 use chrono::prelude::*;
+use inquire::{CustomType, DateSelect, Text};
+use inquire::formatter::StringFormatter;
+use inquire::ui::{RenderConfig, Styled};
+use inquire::validator::StringValidator;
+use inquire::error::{InquireError, InquireResult};
 
-fn main() {
-    let task = Task::new("Meet Dave").date(Utc::today());
+fn main() -> Result<(), Box<dyn Error>>{
+    let task = prompt_task()?;
+    println!("{:?}", task);
 
-    println!("{:#?}", task);
+    Ok(())
+}
+
+fn prompt_task() -> Result<Task, Box<dyn Error>> {
+    let name = prompt_name()?;
+
+    let due = prompt_due()?;
+
+    let task_date = if let Some(date) = due {
+        let time = prompt_time()?;
+        if let Some(local_time) = time {
+            Some(date.and_time(local_time))
+        } else {
+            Some(date.and_hms(0, 0, 0))
+        }
+    } else {
+        None
+    };
+
+    if let Some(due) = task_date {
+        Ok(
+            Task::new(name)
+                .with_date_time(Utc.from_local_datetime(&due).unwrap())
+        )
+    } else {
+        Ok(Task::new(name))
+    }
+}
+
+fn prompt_time() -> InquireResult<Option<NaiveTime>> {
+    let time_style = RenderConfig::default_colored()
+        .with_canceled_prompt_indicator(Styled::new("00:00:00"));
+    CustomType::<NaiveTime>::new("Time:")
+        .with_default((NaiveTime::from_hms(0, 0, 0), &|x| format!("{}", x)))
+        .with_placeholder("HH:MM:SS")
+        .with_parser(&|x| {
+            match NaiveTime::parse_from_str(x, "%H:%M") {
+                Ok(time) => Ok(time),
+                Err(_) => Err(()),
+            }
+        })
+        .with_render_config(time_style)
+        .prompt_skippable()
+}
+
+fn prompt_due() -> InquireResult<Option<NaiveDate>> {
+    let date_style = RenderConfig::default_colored()
+        .with_canceled_prompt_indicator(Styled::new("As soon as possible"));
+
+    DateSelect::new("Due date:")
+        .with_help_message("Press ESC to set task to be due as soon as possible")
+        .with_min_date(Utc::today().naive_utc())
+        .with_vim_mode(true)
+        .with_render_config(date_style)
+        .prompt_skippable()
+}
+
+fn prompt_name() -> Result<String, Box<dyn Error>> {
+    let name_validator: StringValidator = &|input: &str| {
+        if input.chars().any(|x| !x.is_whitespace()) {
+            Ok(())
+        } else {
+            Err(String::from("Name cannot be empty"))
+        }
+    };
+
+    let name_formatter: StringFormatter = &|input: &str| input.trim().to_owned();
+    loop {
+        let name = Text::new("Task name:")
+            .with_help_message("Enter the name of the task")
+            .with_validator(name_validator)
+            .with_formatter(name_formatter)
+            .prompt();
+
+        match name {
+            Ok(n) => return Ok(name_formatter(&n)),
+            Err(InquireError::OperationCanceled) => {},
+            Err(e) => return Err(Box::new(e)),
+        }
+    };
 }
--- src/task.rs
+++ src/task.rs
@@ -27,15 +27,13 @@ impl Task {
     /// use today::Task;
     /// use chrono::prelude::*;
     ///
-    /// let task = Task::new("Meet Dave").date(Utc.ymd(2020, 2, 23));
+    /// let task = Task::new("Meet Dave").with_date(Utc.ymd(2020, 2, 23));
     ///
     /// assert_eq!(task.due(), Some(&Utc.ymd(2020, 2, 23).and_hms(0, 0, 0)));
     /// ```
-    pub fn date(self, due: Date<Utc>) -> Self {
-        Self {
-            due: Some(due.and_hms(0, 0, 0)),
-            .. self
-        }
+    pub fn with_date(mut self, due: Date<Utc>) -> Self {
+        self.due = Some(due.and_hms(0, 0, 0));
+        self
     }
 
     /// Add a due date and time in UTC for a task
@@ -43,24 +41,20 @@ impl Task {
     /// use today::Task;
     /// use chrono::prelude::*;
     ///
-    /// let task = Task::new("Meet Dave").date_time(Utc.ymd(2020, 2, 23).and_hms(15, 30, 00));
+    /// let task = Task::new("Meet Dave").with_date_time(Utc.ymd(2020, 2, 23).and_hms(15, 30, 00));
     ///
     /// assert_eq!(task.due(), Some(&Utc.ymd(2020, 2, 23).and_hms(15, 30, 00)));
     /// ```
-    pub fn date_time(self, due: DateTime<Utc>) -> Self {
-        Self {
-            due: Some(due),
-            .. self
-        }
+    pub fn with_date_time(mut self, due: DateTime<Utc>) -> Self {
+        self.due = Some(due);
+        self
     }
 
     /// Add a time to the task if date has been set first.
     /// If `due` is None then this has no effect
-    pub fn and_time(self, time: NaiveTime) -> Self {
-        Self {
-            due: self.due.map(|x| x.date().and_time(time)).flatten(),
-            .. self
-        }
+    pub fn and_time(mut self, time: NaiveTime) -> Self {
+        self.due = self.due.map(|x| x.date().and_time(time)).flatten();
+        self
     }
 
     /// Get the name of the task
