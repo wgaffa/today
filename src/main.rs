80b1902a0a937f6846678dd21b7532d07ccb80ef
Patrik Maunus <subscription@skriptladan.se>
2021-10-18 06:17:18 +0200

    Check for environment variable
    Now checks the environment variable TODAY_CONFIG_PATH
    for the path to config files, if not present it checks
    the XDG config dir

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
-use std::{collections::HashMap, error::Error};
+use std::{
+    collections::HashMap,
+    env::{self, VarError},
+    error::Error,
+    fs,
+    io::{self, ErrorKind},
+    path::{Path, PathBuf}
+};
 
 use today::TaskManager;
 
 mod ui;
 
 fn main() -> Result<(), Box<dyn Error>>{
+    let conf_path = env::var("TODAY_CONFIG_PATH")
+        .and_then(|x| {
+            match fs::metadata(&x) {
+                Ok(_meta) => Ok(x),
+                Err(_) => Err(VarError::NotPresent),
+            }
+        })
+        .or_else(|_error| {
+            if let Some(path) = config_path() {
+                match setup_config(&path) {
+                    Ok(()) => Ok(path.to_str().unwrap().to_owned()),
+                    Err(e) => Err(e),
+                }
+            } else {
+                todo!()
+            }
+        })?;
+    println!("{:?}", conf_path);
+
     let mut tasks = TaskManager::new();
     let mut dispatcher: HashMap<ui::MenuOption, fn(&mut TaskManager) -> Result<(), Box<dyn Error>>> = HashMap::new();
     dispatcher.insert(ui::MenuOption::Add, |tasks| {
@@ -29,4 +55,30 @@ fn main() -> Result<(), Box<dyn Error>>{
     }
 
     Ok(())
+}
+
+fn config_path() -> Option<PathBuf> {
+    let config_path = dirs::config_dir();
+
+    const APP_ID: &str = "today";
+    config_path.and_then(|mut x| {
+        x.push(APP_ID);
+        Some(x)
+    })
+}
+
+fn setup_config<P: AsRef<Path>>(path: P) -> Result<(), io::Error> {
+    match fs::metadata(&path) {
+        Err(e) if e.kind() == ErrorKind::NotFound => {
+            fs::create_dir_all(&path)
+        },
+        Ok(meta) => {
+            if meta.is_dir() {
+                Ok(())
+            } else {
+                todo!()
+            }
+        }
+        Err(e) => Err(e),
+    }
 }
